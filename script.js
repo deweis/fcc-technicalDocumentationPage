559dd75233f6b9ec48bae789fe6dffe4be80d773
Dennis <deweis@gmx.ch>
2018-09-02 12:39:55 +0200

    Add icon to show sidebar

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-/* Set the width of the side navigation to 0 */
+/* Open the sidenav */
+function openNav() {
+  document.getElementById('navbar').style.width = '250px';
+  document.getElementById('main-doc').style.marginLeft = '250px';
+  document.getElementById('menu-icon').style.display = 'none';
+}
+
+/* Close/hide the sidenav */
 function closeNav() {
   document.getElementById('navbar').style.width = '0';
   document.getElementById('main-doc').style.marginLeft = '0';
+  document.getElementById('menu-icon').style.display = 'block';
 }

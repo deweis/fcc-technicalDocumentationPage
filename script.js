cad35559d934c26c2ab23c9cd60ef70f90d5945a
Dennis <deweis@gmx.ch>
2018-09-02 12:10:36 +0200

    Add icon to hide sidebar

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById('navbar').style.width = '0';
+  document.getElementById('main-doc').style.marginLeft = '0';
+}

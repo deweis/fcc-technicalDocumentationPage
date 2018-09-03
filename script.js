0f239317579bf6039d3426957be4f86a0359b19c
Dennis <deweis@gmx.ch>
2018-09-03 11:29:50 +0200

    Close sidenav when anchor link clicked on mobile devices

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 /* Open the sidenav */
 function openNav() {
-  document.getElementById('navbar').style.width = '250px';
-  document.getElementById('main-doc').style.marginLeft = '250px';
+  if (window.matchMedia('(max-device-width: 500px)').matches) {
+    document.getElementById('navbar').style.width = '100%';
+
+    /* Thank you:
+    *  https://medium.com/@byrayray/5-ways-to-loop-over-dom-elements-from-queryselectorall-in-javascript-55bd66ca4128 */
+    document.querySelectorAll('.anchor-link')
+            .forEach(link => link.addEventListener('click', closeNav));
+  } else {
+    document.getElementById('navbar').style.width = '250px';
+    document.getElementById('main-doc').style.marginLeft = '250px';
+  }
+
   document.getElementById('menu-icon').style.display = 'none';
 }
 
